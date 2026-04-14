213da8249dc2a21b614dc6342254d4275cd92d6d
akatoyo <217076103+AKATOYO@users.noreply.github.com>
2026-04-14 14:13:52 -0500

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+
+// supabase.js
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = "https://hwtkhblqcmptbkebiijx.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsaW9ocHJ6cXh6cHl5cnB2bHZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxOTIyNTcsImV4cCI6MjA5MTc2ODI1N30.vvWoWAnHbfmZMEDWTKV8aGs6OsTKjpMam1h2OXVCjQI";
+
+const client = createClient(SUPABASE_URL, SUPABASE_KEY);

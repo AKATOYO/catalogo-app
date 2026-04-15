2b085834464c376693259fe12412fd522058361b
akatoyo <217076103+AKATOYO@users.noreply.github.com>
2026-04-14 22:48:27 -0500

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,14 @@
+// supabase.js
+export const SUPABASE_URL = "https://hwtkhblqcmptbkebiijx.supabase.co";
+export const SUPABASE_KEY = "yliohprzqxzpyyrpvlvh";
+
+export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function obtenerProductos() {
+  const { data, error } = await client.from("productos").select("*");
+  if (error) {
+    console.error("Error al cargar productos:", error);
+    return [];
+  }
+  return data;
+}

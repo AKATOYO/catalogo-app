4109136802824d38e531f3277217a7145596d476
akatoyo <217076103+AKATOYO@users.noreply.github.com>
2026-04-15 00:26:18 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,14 +1,3 @@
-// supabase.js
-export const SUPABASE_URL = "https://hwtkhblqcmptbkebiijx.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_jWnZtBxthINwZnn2NDS6wg_wour17Cc";
-
-export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-export async function obtenerProductos() {
-  const { data, error } = await client.from("productos").select("*");
-  if (error) {
-    console.error("Error al cargar productos:", error);
-    return [];
-  }
-  return data;
-}
+const SUPABASE_URL = "https://hwtkhblqcmptbkebiijx.supabase.co";
+    const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ...";
+    const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);

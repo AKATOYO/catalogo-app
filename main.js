1c370c845d2b3ad1f41cf2349a6523a2f67cdbf4
akatoyo <217076103+AKATOYO@users.noreply.github.com>
2026-04-15 11:35:11 -0500

    Update Supabase key in main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
     // Si lo deseas, puedo también optimizarlo o dividirlo en módulos
 
     const SUPABASE_URL = "https://hwtkhblqcmptbkebiijx.supabase.co";
-    const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ...";
+    const SUPABASE_KEY = "sb_publishable_jWnZtBxthINwZnn2NDS6wg_wour17Cc";
     const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
     let productosOriginales = [];

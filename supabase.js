d29740ef314b1616657faa312f766c33a39a58f7
akatoyo <217076103+AKATOYO@users.noreply.github.com>
2026-04-15 19:53:02 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,28 @@
-const supabaseUrl = 'https://yliohprzqxzpyyrpvlvh.supabase.co';
-const supabaseAnonKey = 'sb_publishable_jWnZtBxthINwZnn2NDS6wg_wour17Cc'; // Reemplaza con tu clave anon si es diferente
 
-const supabase = supabase.createClient(supabaseUrl, supabaseAnonKey);
+
+<script type="module">
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabase = createClient(
+  'https://yliohprzqxzpyyrpvlvh.supabase.co',
+  'sb_publishable_jWnZtBxthINwZnn2NDS6wg_wour17Cc'
+)
+
+async function cargarUsuarios() {
+  const { data, error } = await supabase
+    .from('usuarios')
+    .select('*')
+
+  if (error) {
+    console.error('Error al leer usuarios:', error)
+  } else {
+    console.log('Usuarios:', data)
+  }
+}
+
+cargarUsuarios()
+
+
 
  let productos = [];
 
@@ -43,3 +64,5 @@ async function fetchProductos() {
 }
 
 fetchProductos();
+
+</script>

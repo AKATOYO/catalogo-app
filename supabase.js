91b9cd877bdc5be9c36db25ab5f76d1b6c0b3f64
akatoyo <217076103+AKATOYO@users.noreply.github.com>
2026-04-16 02:29:35 -0500

    Refactor obtenerProductos to improve filtering logic
    Refactor obtenerProductos function to use ilike for filtering.

--- supabase.js
+++ supabase.js
@@ -5,21 +5,19 @@ const supabaseUrl = 'https://yliohprzqxzpyyrpvlvh.supabase.co';
 const supabaseKey = 'sb_publishable_jWnZtBxthINwZnn2NDS6wg_wour17Cc';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Obtener productos filtrando por nombre o descripcion
+
+// Función para obtener productos filtrando por nombre
 export async function obtenerProductos(filtro = '') {
-  let query = supabase
-    .from('productos')
-    .select('*');
-
+  let query = supabase.from('productos').select('*');
   if (filtro) {
-    query = query.or(`nombre.ilike.%${filtro}%` , `descripcion.ilike.%${filtro}%`);
+    query = query.ilike('nombre',`%${filtro}%`);
+    query = query.ilike('descripcion',`%${filtro}%`);
   }
-
   const { data, error } = await query;
+
   if (error) {
-    console.error(error);
+    console.error('Error al obtener productos:', error);
     return [];
   }
-
   return data;
 }
